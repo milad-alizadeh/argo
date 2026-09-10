f458e29625222a5e5385d893ca3821dcc6940012
Milad Alizadeh <milad.alizadeh@trili.tech>
2026-09-10 19:56:29 +0100

    Broaden composer stack elevation

--- apps/desktop/src/renderer/modules/composer-prototype/ComposerPrototype.tsx
+++ apps/desktop/src/renderer/modules/composer-prototype/ComposerPrototype.tsx
@@ -913,7 +913,7 @@ function AlignmentGrid({ visible, onToggle }: { visible: boolean; onToggle: () =
 function AnimatedHeight({ children }: { children: ReactNode }) {
   return (
     <div className="relative z-10 mx-auto w-full max-w-4xl">
-      <div className="relative z-10 rounded-2xl [&>*]:!border-border/40 [&_button]:!text-sm [&_button]:!font-normal [&_span]:!text-sm [&_span]:!font-normal">
+      <div className="relative z-10 rounded-2xl [&>*]:!border-border/40 [&>*]:!shadow-[0_10px_32px_-16px_rgba(0,0,0,0.24)] [&_button]:!font-normal [&_span]:!font-normal">
         {children}
       </div>
     </div>
@@ -1236,7 +1236,7 @@ export function ComposerPrototype() {
         <Transcript messages={messages} />
       </div>
       <div className="relative shrink-0 bg-gradient-to-t from-background via-background to-transparent px-8 pt-8 pb-12">
-      <div className="composer-queue-stack mx-auto w-full max-w-4xl [&>*]:!border-border/40 [&>*]:!shadow-sm [&_button]:!text-sm [&_button]:!font-normal [&_span]:!text-sm [&_span]:!font-normal">
+      <div className="composer-queue-stack mx-auto w-full max-w-4xl [&>*]:!border-border/40 [&>*]:!shadow-[0_10px_32px_-16px_rgba(0,0,0,0.24)] [&_button]:!font-normal [&_span]:!font-normal">
           <QueuePreview messages={queuedMessages} layout="attached-stack" onSteer={steerQueuedMessage} onRemove={removeQueuedMessage} onEdit={editQueuedMessage} onReorder={reorderQueuedMessage} latestQueuedId={latestQueuedId} isAdding={isQueueAnimating} />
         </div>
         <AnimatedHeight>
@@ -1299,7 +1299,7 @@ export function ComposerPrototype() {
           </InputGroup>
           </form>
         </AnimatedHeight>
-      <div className="relative z-0 mx-auto -mt-2 w-[calc(100%-2rem)] max-w-[calc(56rem-2rem)] [&>*]:!border-border/40 [&>*]:!px-4 [&>*]:!shadow-sm [&_button]:!text-sm [&_button]:!font-normal [&_span]:!text-sm [&_span]:!font-normal">
+      <div className="relative z-0 mx-auto -mt-2 w-[calc(100%-2rem)] max-w-[calc(56rem-2rem)] [&>*]:!border-border/40 [&>*]:!px-4 [&>*]:!shadow-[0_10px_32px_-16px_rgba(0,0,0,0.24)] [&_button]:!font-normal [&_span]:!font-normal">
           <ContextSurface state={state} layout="attached" tone={contextTone} />
         </div>
       </div>
