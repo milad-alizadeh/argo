2cd6bd4c578550f04012db1474fbd104db4f4c5a
Milad Alizadeh <milad.alizadeh@trili.tech>
2026-09-10 20:02:56 +0100

    Unify composer borders and remove focus treatment

--- apps/desktop/src/renderer/modules/composer-prototype/ComposerPrototype.tsx
+++ apps/desktop/src/renderer/modules/composer-prototype/ComposerPrototype.tsx
@@ -913,7 +913,7 @@ function AlignmentGrid({ visible, onToggle }: { visible: boolean; onToggle: () =
 function AnimatedHeight({ children }: { children: ReactNode }) {
   return (
     <div className="relative z-10 mx-auto w-full max-w-4xl">
-      <div className="relative z-10 rounded-2xl [&>*]:!border-border/40 [&_button]:!font-normal [&_span]:!font-normal">
+      <div className="relative z-10 rounded-2xl [&>*]:!border-border/60 [&_*]:!border-border/60 [&_button]:!font-normal [&_span]:!font-normal">
         {children}
       </div>
     </div>
@@ -1236,12 +1236,12 @@ export function ComposerPrototype() {
         <Transcript messages={messages} />
       </div>
       <div className="relative shrink-0 bg-gradient-to-t from-background via-background to-transparent px-8 pt-8 pb-12">
-      <div className="composer-queue-stack mx-auto w-full max-w-4xl [&>*]:!border-border/40 [&>*]:!shadow-[0_10px_32px_-16px_rgba(0,0,0,0.3)] [&_button]:!font-normal [&_span]:!font-normal [&_svg]:!size-4">
+      <div className="composer-queue-stack mx-auto w-full max-w-4xl [&>*]:!border-border/60 [&>*]:!shadow-[0_10px_32px_-16px_rgba(0,0,0,0.3)] [&_*]:!border-border/60 [&_button]:!font-normal [&_span]:!font-normal [&_svg]:!size-4">
           <QueuePreview messages={queuedMessages} layout="attached-stack" onSteer={steerQueuedMessage} onRemove={removeQueuedMessage} onEdit={editQueuedMessage} onReorder={reorderQueuedMessage} latestQueuedId={latestQueuedId} isAdding={isQueueAnimating} />
         </div>
         <AnimatedHeight>
           <form
-            className="relative z-10 mx-auto w-full max-w-4xl [&>*]:!border-border/40 [&>*]:!shadow-[0_10px_32px_-16px_rgba(0,0,0,0.3)] [&_svg]:!size-4"
+            className="relative z-10 mx-auto w-full max-w-4xl [&>*]:!border-border/60 [&>*]:!shadow-[0_10px_32px_-16px_rgba(0,0,0,0.3)] [&>*]:focus-within:!border-border/60 [&>*]:focus-within:!outline-none [&>*]:focus-within:!ring-0 [&_*]:!border-border/60 [&_textarea]:focus:!outline-none [&_textarea]:focus-visible:!outline-none [&_textarea]:focus-visible:!ring-0 [&_svg]:!size-4"
             onSubmit={(event) => {
               event.preventDefault()
               send()
@@ -1299,7 +1299,7 @@ export function ComposerPrototype() {
           </InputGroup>
           </form>
         </AnimatedHeight>
-      <div className="relative z-0 mx-auto -mt-2 w-[calc(100%-2rem)] max-w-[calc(56rem-2rem)] [&>*]:!border-border/40 [&>*]:!px-4 [&>*]:!shadow-[0_10px_32px_-16px_rgba(0,0,0,0.3)] [&_button]:!font-normal [&_span]:!font-normal [&_svg]:!size-4">
+      <div className="relative z-0 mx-auto -mt-2 w-[calc(100%-2rem)] max-w-[calc(56rem-2rem)] [&>*]:!border-border/60 [&>*]:!px-4 [&>*]:!shadow-[0_10px_32px_-16px_rgba(0,0,0,0.3)] [&_*]:!border-border/60 [&_button]:!font-normal [&_span]:!font-normal [&_svg]:!size-4">
           <ContextSurface state={state} layout="attached" tone={contextTone} />
         </div>
       </div>
