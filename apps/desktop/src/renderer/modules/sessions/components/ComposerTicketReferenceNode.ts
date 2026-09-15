82345952e98b57e8e63d3cc1454fa12ac9e59c89
Milad Alizadeh <milad.alizadeh@trili.tech>
2026-09-15 02:54:49 +0100

    Use middle alignment for inline ticket icons

--- apps/desktop/src/renderer/modules/sessions/components/ComposerTicketReferenceNode.ts
+++ apps/desktop/src/renderer/modules/sessions/components/ComposerTicketReferenceNode.ts
@@ -36,7 +36,7 @@ export class ComposerTicketReferenceNode extends TextNode {
     const element = super.createDOM(config)
     const icon = element.ownerDocument.createElement('img')
     icon.alt = ''
-    icon.className = 'relative -top-0.5 mr-1 inline-block size-3.5 align-middle dark:invert'
+    icon.className = 'mr-1 inline-block size-3.5 align-middle dark:invert'
     icon.contentEditable = 'false'
     icon.src = ticketProviderIconSource[this.__provider]
     element.className = 'mx-0.5 cursor-pointer font-semibold text-foreground'
